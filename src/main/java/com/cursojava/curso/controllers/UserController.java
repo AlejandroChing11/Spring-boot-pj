c2bb2f9e45d2a68c8fb23ec0e1676354c986490b
Alejandro Ching <alejandroching2004@hotmail.com>
2023-01-27 12:13:38 -0500

    Adding ajax and users

--- src/main/java/com/cursojava/curso/controllers/UserController.java
+++ src/main/java/com/cursojava/curso/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,11 +15,46 @@ public class UserController {
     public User getUser (@PathVariable Long id) {
         User user = new User(); //Creacion del objeto
         user.setId(id);
+        user.setName("Mei Li"); //Seteamos el name
+        user.setLastName("Ching"); //Seteamos last name OJO TODO AL OBJETO
+        user.setEmail("MeiChill@hotmail.com"); //Seteamos el email
+        user.setPhoneNumber("24567343"); //Seteamos el phone number
+        return user; //Retornamos en formato JSON el objeto user
+    }
+
+    @RequestMapping(value = "usuario")
+    public List<User> getUsers (@PathVariable Long id) {
+
+        List <User> usuarios = new ArrayList<>(); //Creamos el array list para nuestros usuarios
+
+        User user = new User(); //Creacion del objeto
+        user.setId(id);
         user.setName("Alejandro"); //Seteamos el name
         user.setLastName("Ching"); //Seteamos last name OJO TODO AL OBJETO
         user.setEmail("Alejandroching2004@hotmail.com"); //Seteamos el email
         user.setPhoneNumber("2456788"); //Seteamos el phone number
-        return user; //Retornamos en formato JSON el objeto user
+
+
+        User user2 = new User(); //Creacion del objeto
+        user2.setId(id);
+        user2.setName("Sandra"); //Seteamos el name
+        user2.setLastName("Franco"); //Seteamos last name OJO TODO AL OBJETO
+        user2.setEmail("SandraFranco@hotmail.com"); //Seteamos el email
+        user2.setPhoneNumber("24567343"); //Seteamos el phone number
+
+
+        User user3 = new User(); //Creacion del objeto
+        user3.setId(id);
+        user3.setName("Marlon"); //Seteamos el name
+        user3.setLastName("Ching"); //Seteamos last name OJO TODO AL OBJETO
+        user3.setEmail("Marlon@hotmail.com"); //Seteamos el email
+        user3.setPhoneNumber("2456343"); //Seteamos el phone number
+
+        usuarios.add(user);
+        usuarios.add(user2);
+        usuarios.add(user3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usuario2")
