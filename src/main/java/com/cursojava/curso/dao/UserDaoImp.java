795cef5c27e1d3a4a9f535fe617e92bf32797bf0
Alejandro Ching <alejandroching2004@hotmail.com>
2023-01-30 23:10:34 -0500

    Connecting to MySQL v2

--- src/main/java/com/cursojava/curso/dao/UserDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UserDaoImp.java
@@ -3,13 +3,13 @@ package com.cursojava.curso.dao;
 import com.cursojava.curso.models.User;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.List;
 
 @Repository
 @Transactional
+
 public class UserDaoImp implements UserDAO{
-
-
     @Override
     public List<User> getUsers() {
         return null;
