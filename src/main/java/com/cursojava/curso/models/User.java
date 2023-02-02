9e23358b563ed0d617002157995318bd81372fe4
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-01 21:57:27 -0500

    Connecting and fixing querys v2

--- src/main/java/com/cursojava/curso/models/User.java
+++ src/main/java/com/cursojava/curso/models/User.java
@@ -1,11 +1,32 @@
 package com.cursojava.curso.models;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+
 public class User {
+    @Getter @Setter @Column(name = "id")
     private Long id;
+
+    @Getter @Setter @Column(name = "nombre")
     private String name;
+
+    @Getter @Setter @Column(name = "apellido")
     private String lastName;
+
+    @Getter @Setter @Column(name = "email")
     private String email;
+
+    @Getter @Setter @Column(name = "telefono")
     private String phoneNumber;
+
+    @Getter @Setter @Column(name = "password")
     private String password;
 
     public Long getId() {
@@ -16,43 +37,5 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
