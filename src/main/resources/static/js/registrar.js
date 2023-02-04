c50779c63b09ec8f3fca9e4c9067ccd740913e12
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-04 17:08:17 -0500

    Register users logic v5

--- src/main/resources/static/js/registrar.js
+++ src/main/resources/static/js/registrar.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
 async function registrarUsuario() {
     let datos = {};
-    datos.name = document.getElementById('txtNombre').value;
-    datos.LastName = document.getElementById('txtApellido').value;
+    datos.nombre = document.getElementById('txtNombre').value;
+    datos.apellido = document.getElementById('txtApellido').value;
     datos.email = document.getElementById('txtEmail').value;
     datos.password = document.getElementById('txtPassword').value;
 
@@ -17,14 +17,14 @@ async function registrarUsuario() {
         return;
     }
 
-    const request = await fetch("api/usuarios", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(datos)
-      });
+    const request = await fetch('api/usuarios', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    });
 }
 
 
