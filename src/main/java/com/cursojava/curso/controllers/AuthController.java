6f9077901edd6c89d83436a3257714911940be80
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-13 14:55:05 -0500

    implementing JWT login

--- src/main/java/com/cursojava/curso/controllers/AuthController.java
+++ src/main/java/com/cursojava/curso/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.dao.UserDAO;
 import com.cursojava.curso.models.User;
+import com.cursojava.curso.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,15 @@ public class AuthController {
     @Autowired
     private UserDAO userDao;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public String login (@RequestBody User usuario) {
        if (userDao.verificarCredenciales(usuario)){
+
+
+
            return "OK";
        } else {
            return "ERROR";
